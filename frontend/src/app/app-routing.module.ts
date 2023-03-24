a18fc8ee3d768a9bb1bb9ebd8798ee4789ddfd8e
Deepak Bisht <deepakbisht84@gmail.com>
2023-03-24 15:51:18 +0530

    Register Page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
 
 const routes: Routes = [
   {
@@ -29,6 +30,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent
   }
 ];
 
