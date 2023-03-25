3f4baf50281412300224d08310c89ee25ec51a0c
Deepak Bisht <deepakbisht842@gmail.com>
2023-03-25 15:29:38 +0530

    changes port from 4200 to 5000 in server.ts file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,31 +1,33 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import path from 'path';
-import express from 'express';
-import cors from 'cors';
-import foodRouter from './routers/food.router';
-import userRouter from './routers/user.router';
-import { dbConnect } from './configs/database.config';
+import path from "path";
+import express from "express";
+import cors from "cors";
+import foodRouter from "./routers/food.router";
+import userRouter from "./routers/user.router";
+import { dbConnect } from "./configs/database.config";
 dbConnect();
 
 const app = express();
 //localhost:4200- frontend
 //localhost:5000- backend
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: ["http://localhost:4200"]
-}));
+    origin: ["http://localhost:5000"],
+  })
+);
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("Website served on http://localhost:" + port);
-})
+  console.log("Website served on http://localhost:" + port);
+});
